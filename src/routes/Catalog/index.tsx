4d38b345d0ad15cab85aada74cb8fc7c16b03e24
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-03-30 22:14:25 -0300

    Catalog cards

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,21 +1,29 @@
-import './styles.css';
+import "./styles.css";
 import Header from "../../components/Header";
-import SearchBar from '../../components/SearchBar';
+import SearchBar from "../../components/SearchBar";
+import Card from "../../components/Card";
 
 export default function Catalog() {
   return (
     <>
       <Header />
-      <main className='ct-catalog-main'>
-        <section id="ct-catalog-section" className='ct-container'>
-            <div className='ct-search-bar-container'>
-                <SearchBar />
-            </div>
-            <div className='ct-catalog-cards-container'>
-                <div>
-                    Teste
-                </div>
-            </div>
+      <main className="ct-catalog-main">
+        <section id="ct-catalog-section" className="ct-container">
+          <div className="ct-search-bar-container">
+            <SearchBar />
+          </div>
+          <div className="ct-catalog-cards-container">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
         </section>
       </main>
     </>
